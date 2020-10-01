b611035bc91b1b056956cefd4976ffae4efe3363
Balázs Indig <indba@digitus.itk.ppke.hu>
2020-10-01 21:55:16 +0200

    Update Makefile and minor cosmetic fixes

--- emzero/__main__.py
+++ emzero/__main__.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
-# -*- coding: utf-8 -*-
+# -*- coding: utf-8, vim: expandtab:ts=4 -*-
 
-from xtsv import build_pipeline, parser_skeleton
+from xtsv import build_pipeline, parser_skeleton, jnius_config
 
 
 def main():
@@ -9,6 +9,8 @@ def main():
     argparser = parser_skeleton(description='emZero - a module for marking zero pronouns'
                                             ' in dependency parsed sentences')
     opts = argparser.parse_args()
+
+    jnius_config.classpath_show_warning = opts.verbose  # Suppress warning.
 
     # Set input and output iterators...
     if opts.input_text is not None:
@@ -32,7 +34,19 @@ def main():
     tools = [(em_zero, ('zero', 'emZero'))]
 
     # Run the pipeline on input and write result to the output...
-    output_iterator.writelines(build_pipeline(input_data, used_tools, tools, presets))
+    output_iterator.writelines(build_pipeline(input_data, used_tools, tools, presets, opts.conllu_comments))
+
+    # TODO this method is recommended when debugging the tool
+    # Alternative: Run specific tool for input (still in emtsv format):
+    # from xtsv import process
+    # from emdummy import EmDummy
+    # output_iterator.writelines(process(input_data, EmDummy(*em_dummy[3], **em_dummy[4])))
+
+    # Alternative2: Run REST API debug server
+    # from xtsv import pipeline_rest_api, singleton_store_factory
+    # app = pipeline_rest_api('TEST', tools, {},  conll_comments=False, singleton_store=singleton_store_factory(),
+    #                         form_title='TEST TITLE', doc_link='https://github.com/dlt-rilmta/emdummy')
+    # app.run()
 
 
 if __name__ == '__main__':
--- emzero/emzero.py
+++ emzero/emzero.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-# -*- coding: utf-8 -*-
+# -*- coding: utf-8, vim: expandtab:ts=4 -*-
 
 """
     author: Noémi Vadász
